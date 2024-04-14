71643efab292973a88422217c1e296f615032c01
Alan Rodz <s061199@hotmail.com>
2024-04-14 17:55:52 -0600

    Refactor package.json to update dependencies and remove react-plotly.js

--- src/app/math.ts
+++ src/app/math.ts
@@ -1,35 +1,47 @@
 import { DataPoint, HyperParams } from './type';
 
 // ********************************************************************************
+// == Data ========================================================================
+const generateRandomNumber = ({ min, max }: { min: number; max: number }) => Math.random() * (max - min) + min;
+
 export const generateData = ({ clusterCount, pointsPerCluster, clusterRadius }: HyperParams) => {
   const points: DataPoint[] = [];
   const labels = Array.from({ length: clusterCount }, (_, i) => i);
   const labelColors = labels.map((label) => `hsl(${(label * 360) / clusterCount}, 70%, 50%)`);
 
   for (let i = 0; i < clusterCount; i++) {
-    const clusterX = Math.random() * 10;
-    const clusterY = Math.random() * 10;
+    const clusterX = generateRandomNumber({ min: 0, max: 10 });
+    const clusterY = generateRandomNumber({ min: 0, max: 10 });
+
     for (let j = 0; j < pointsPerCluster; j++) {
-      const x = clusterX + (Math.random() - 0.5) * clusterRadius;
-      const y = clusterY + (Math.random() - 0.5) * clusterRadius;
-      const label = labels[i];
-      points.push({ x, y, label });
+      const x = clusterX + generateRandomNumber({ min: -0.5, max: 0.5 }) * clusterRadius;
+      const y = clusterY + generateRandomNumber({ min: -0.5, max: 0.5 }) * clusterRadius;
+      points.push({ x, y, label: labels[i] });
     }
   }
+
   return { labels, labelColors, points }
 };
 
+// == Distance ====================================================================
 export const computeEuclideanDistance = (p1: DataPoint, p2: DataPoint) => Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
 
-export const knn = (points: DataPoint[], classifiedPoint: DataPoint, k: number) => {
+// == KNN =========================================================================
+export const classifyUsingKNN = (points: DataPoint[], classifiedPoint: DataPoint, nearestNeighborAmount: number) => {
   const distances = points.map((d) => ({ point: d, distance: computeEuclideanDistance(classifiedPoint, d) }));
-  distances.sort((a, b) => a.distance - b.distance);
-  const nearestNeighbors = distances.slice(0, k);
+  const sortedDistances = { ...distances.sort((a, b) => a.distance - b.distance) };
+  const nearestNeighbors = sortedDistances.slice(0, nearestNeighborAmount);
 
-  const counts = nearestNeighbors.reduce((acc, curr) => {
-    acc[curr.point.label] = (acc[curr.point.label] || 0) + 1;
+  const labelCounts = nearestNeighbors.reduce((acc, curr) => {
+    acc[curr.point.label] = (acc[curr.point.label] ?? 0) + 1;
     return acc;
   }, {} as { [key: string]: number });
 
-  return Object.keys(counts).reduce((prev, curr) => (counts[curr] > counts[prev] ? curr : prev), Object.keys(counts)[0]);
+  const keys = Object.keys(labelCounts);
+  return keys.reduce((acc, currentIdx) =>
+  (
+    labelCounts[currentIdx] > labelCounts[acc]
+      ? currentIdx
+      : acc
+  ), keys[0]);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 'use client';
 
-import { Button, Center, Input, Text } from '@chakra-ui/react';
-import { PlotHoverEvent } from 'plotly.js';
-import { useEffect, useState } from 'react';
-import Plot from 'react-plotly.js';
+import { Box, Button, Center, Input, Text } from '@chakra-ui/react';
+import { BarController, BarElement, CategoryScale, ChartData, Chart as ChartJS, Legend, LineController, LineElement, LinearScale, PointElement, Tooltip } from 'chart.js';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { Chart } from 'react-chartjs-2';
 
-import { generateData, knn } from './math';
-import { DataPoint, HyperParams } from './type';
+import { generateData } from './math';
 
 // ********************************************************************************
+// == Constant ====================================================================
+ChartJS.register(BarController, BarElement, CategoryScale, Legend, LineController, LinearScale, LineElement, PointElement, Tooltip);
+
+const MAX_CLUSTER_COUNT = 10;
+const MIN_CLUSTER_COUNT = 2;
+
+const MAX_POINTS_PER_CLUSTER = 200;
+const MIN_POINTS_PER_CLUSTER = 2;
+
+const MAX_CLUSTER_RADIUS = 10;
+const MIN_CLUSTER_RADIUS = 0.1;
+
 // == Component ===================================================================
 const LandingPageComponent: React.FC = () => {
   // -- State ---------------------------------------------------------------------
@@ -18,24 +29,43 @@ const LandingPageComponent: React.FC = () => {
   const [k, setK] = useState(3);
   const [data, setData] = useState(() => generateData({ clusterCount, pointsPerCluster, clusterRadius }));
 
-  const [hoverData, setHoverData] = useState<DataPoint | null>(null);
-
   // -- Effect --------------------------------------------------------------------
   useEffect(() => {
     setData(generateData({ clusterCount, pointsPerCluster, clusterRadius }));
   }, [clusterCount, pointsPerCluster, clusterRadius]);
 
   // -- Handler -------------------------------------------------------------------
-  const handleHover = (event: Readonly<PlotHoverEvent>) => {
-    if (event.points.length > 0) {
-      const pointIndex = event.points[0].pointIndex;
-      setHoverData(data.points[pointIndex]);
-    } else {
-      setHoverData(null);
-    }
+  const handleClusterCountChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = Number(e.target.value);
+    if (value > MAX_CLUSTER_COUNT) { value = MAX_CLUSTER_COUNT; }
+    else if (value < MIN_CLUSTER_COUNT) { value = MIN_CLUSTER_COUNT; }
+    setClusterCount(value);
+  };
+
+  const handlePointsPerClusterChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = Number(e.target.value);
+    if (value > MAX_POINTS_PER_CLUSTER) { value = MAX_POINTS_PER_CLUSTER; }
+    else if (value < MIN_POINTS_PER_CLUSTER) { value = MIN_POINTS_PER_CLUSTER; }
+    setPointsPerCluster(value);
+  };
+
+  const handleClusterRadiusChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = Number(e.target.value);
+    if (value > MAX_CLUSTER_RADIUS) { value = MAX_CLUSTER_RADIUS; }
+    else if (value < MIN_CLUSTER_RADIUS) { value = MIN_CLUSTER_RADIUS; }
+    setClusterRadius(value);
   };
 
   // -- UI ------------------------------------------------------------------------
+  const shownData: ChartData = {
+    datasets: data.labels.map((label, idx) => ({
+      backgroundColor: data.labelColors[idx],
+      borderColor: data.labelColors[idx],
+      borderWidth: 1,
+      data: data.points.filter((point) => point.label === label).map((point) => ({ x: point.x, y: point.y })),
+      label: `Cluster ${label}`,
+    }))
+  };
   return (
     <Center flexDir='column' gap='1em' paddingY='3em'>
       <Text fontSize='2em' fontWeight='bold'>K-Nearest Neighbors</Text>
@@ -44,7 +74,7 @@ const LandingPageComponent: React.FC = () => {
           <Text fontWeight='bold' width='fit-content'>Número de clusters:</Text>
           <Center gap='1em'>
             <Button onClick={() => setClusterCount(Math.max(clusterCount - 1, 2))}>-</Button>
-            <Input isDisabled={true} type='number' value={clusterCount} width='5em' />
+            <Input onChange={(e) => handleClusterCountChange(e)} type='number' value={clusterCount} width='5em' />
             <Button onClick={() => setClusterCount(clusterCount + 1)}>+</Button>
           </Center>
         </Center>
@@ -53,7 +83,7 @@ const LandingPageComponent: React.FC = () => {
           <Text fontWeight='bold' width='fit-content'>Puntos por cluster:</Text>
           <Center gap='1em'>
             <Button onClick={() => setPointsPerCluster(Math.max(pointsPerCluster - 1, 2))}>-</Button>
-            <Input isDisabled={true} type='number' value={pointsPerCluster} width='5em' />
+            <Input onChange={(e) => handlePointsPerClusterChange(e)} type='number' value={pointsPerCluster} width='5em' />
             <Button onClick={() => setPointsPerCluster(pointsPerCluster + 1)}>+</Button>
           </Center>
         </Center>
@@ -63,7 +93,7 @@ const LandingPageComponent: React.FC = () => {
 
           <Center gap='1em'>
             <Button onClick={() => setClusterRadius(Math.max(clusterRadius - 0.1, 0.1))}>-</Button>
-            <Input isDisabled={true} type='number' value={clusterRadius} width='5em' />
+            <Input onChange={(e) => handleClusterRadiusChange(e)} type='number' value={clusterRadius} width='5em' />
             <Button onClick={() => setClusterRadius(clusterRadius + 0.1)}>+</Button>
           </Center>
         </Center>
@@ -72,36 +102,18 @@ const LandingPageComponent: React.FC = () => {
       <Center gap='1em'>
         <Text fontWeight='bold' width='fit-content'>Valor de K:</Text>
         <Button onClick={() => setK(Math.max(k - 1, 1))}>-</Button>
-        <Input isDisabled={true} type='number' value={k} width='5em' />
+        <Input type='number' value={k} width='5em' />
         <Button onClick={() => setK(k + 1)}>+</Button>
       </Center>
 
-      {
-        hoverData && (
-          <div>
-            Punto seleccionado con el mouse: ({hoverData.x.toFixed(2)}, {hoverData.y.toFixed(2)}){' '}
-            con etiqueta: {hoverData.label}
-          </div>
-        )}
-      <div>Predicción en (3, 5): {knn(data.points, { x: 3, y: 5, label: 0 }, k)}</div>
-      <Plot
-        data={[
-          {
-            type: 'scatter',
-            mode: 'markers',
-            x: data.points.map((point) => point.x),
-            y: data.points.map((point) => point.y),
-            text: data.points.map((d) => d.label.toString()),
-            marker: {
-              color: data.points.map((point) => (data.labelColors[point.label]))
-            },
-            hoverinfo: 'text'
-          }
-        ]}
-        layout={{ width: 600, height: 400 }}
-        style={{ border: '1px solid black', borderRadius: '16px', padding: '2em' }}
-        onHover={handleHover}
-      />
+      <Box height='500px' width='600px'>
+        <Chart
+          data={shownData}
+          height={400}
+          width={400}
+          type='scatter'
+        />
+      </Box>
     </Center>
   );
 };
